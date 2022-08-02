27e6bfaf28858df82117731edc931a4b0a79191e
Javier Martinez Balmaceda <javiersmb83@gmail.com>
2022-08-01 22:16:35 -0300

    Fixed templates path

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'    
 ]
 
 MIDDLEWARE = [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC-3'
 
 USE_I18N = True
 
